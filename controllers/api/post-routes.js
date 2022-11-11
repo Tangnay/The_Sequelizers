95f4a41573259ac7b0f76c221946b3dfaf86794b
Gloriana Gonzalez <glogonzalez@Glorianas-MBP.lan>
2022-11-11 14:22:55 -0600

    changes to css in login page, edit post page

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -138,7 +138,7 @@ router.put("/:id", withAuth, (req, res) => {
     Post.update({
             title: req.body.title,
             content: req.body.post_content,
-            image: req.body.image,
+            image: req.file
         }, {
             where: {
                 id: req.params.id,
--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -3,6 +3,7 @@ async function newFormHandler(event) {
 
     const title = document.querySelector('input[name="post_title"]').value;
     const post_content = document.querySelector('textarea[name="post_content"]').value.trim();
+    var display = document.querySelector('#display');
     const file = document.querySelector('#upload').files[0];
 
     console.log(file)
@@ -16,7 +17,7 @@ async function newFormHandler(event) {
         console.log(key, value);
       }
 
-    const response = await fetch(`/api/posts/upload`, {
+    const response = await fetch('/api/posts/upload', {
         method: 'POST',
         body:
         //  {
@@ -26,9 +27,9 @@ async function newFormHandler(event) {
     //         'Content-Type': 'multipart/form-data'
     //      }
     });
-
+    
     if (response.ok) {
-       document.location.replace('/dashboard');
+          document.location.replace('/dashboard');
     } else {
         alert(response.statusText);
     }
