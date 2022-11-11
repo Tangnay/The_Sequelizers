9cf928c6d375fcd4a41ea11c7dce34ad9e9f6c0c
Gloriana Gonzalez <glogonzalez@Glorianas-MBP.lan>
2022-11-10 20:34:24 -0600

    multer

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -30,7 +30,7 @@ async function newFormHandler(event) {
     });
 
     if (response.ok) {
-        // document.location.replace('/dashboard');
+       document.location.replace('/dashboard');
     } else {
         alert(response.statusText);
     }
